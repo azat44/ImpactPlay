9135150d4ddd9a8e7fb0b910d8e4a3c6324eede0
Elias <101352052+eliascending@users.noreply.github.com>
2024-06-28 09:32:40 +0200

    jpush

--- src/components/leaderboardItem/LeaderboardItem.jsx
+++ src/components/leaderboardItem/LeaderboardItem.jsx
@@ -2,6 +2,9 @@ import styles from './LeaderboardItem.module.css';
 const LeaderboardItem = () => {
     return (
         <div className={styles.LeaderboardContainer}>
+            <div class="LeaderboardTop">
+                s
+            </div>
         </div>
     )
 }
--- src/pages/Classement.jsx
+++ src/pages/Classement.jsx
@@ -1,10 +1,13 @@
+import LeaderboardItem from '../components/leaderboardItem/LeaderboardItem';
 import styles from './Classement.module.css';
 
 const Classement = () => {
   return (
     <section>
       <div className={styles.leaderboard}>
-
+        <LeaderboardItem
+        
+        />
       </div>
     </section>
   );
